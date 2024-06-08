32361e56e97794c2bff63d5555613c66a73aa711
tanhoadev <buitanhoadev@gmail.com>
2024-06-08 15:16:59 +0700

    fix bug get data province change

--- src/components/user/checkout/Address.jsx
+++ src/components/user/checkout/Address.jsx
@@ -23,14 +23,13 @@ function Address() {
     const fetchDistricts = (provincesID) => {
         axios.get(`https://online-gateway.ghn.vn/shiip/public-api/master-data/district`,
             {
-                params: { province_id: 201 },
+                params: { province_id: provincesID },
                 headers: {
                     'Content-Type': 'application/json',
                     'Token': 'cd374817-241d-11ef-ae41-f654f1114181'
                 }
             },)
             .then(data => {
-                console.log(data)
                 setDistricts(data.data.data);
             })
             .catch(error => {
@@ -40,7 +39,7 @@ function Address() {
 
     const fetchWards = (districtsID) => {
         axios.get(`https://online-gateway.ghn.vn/shiip/public-api/master-data/ward`, {
-            params: { district_id: 1542 },
+            params: { district_id: districtsID },
             headers: {
                 'Content-Type': 'application/json',
                 'Token': 'cd374817-241d-11ef-ae41-f654f1114181'
@@ -56,34 +55,51 @@ function Address() {
 
     const handleProvinceChange = (event) => {
         fetchDistricts(event.target.value);
+        setDistricts([]);
         setWards([]);
     };
 
     const handleDistrictChange = (event) => {
         fetchWards(event.target.value);
+        setWards([]);
     };
 
     return (
-        <div>
-            <select id='provinces' onChange={handleProvinceChange}>
-                <option value=''>-- Chọn tỉnh/thành phố --</option>
-                {provinces.map(province => (
-                    <option key={province.code} value={province.code}>{province.ProvinceName}</option>
-                ))}
-            </select>
-            <select id='districts' onChange={handleDistrictChange}>
-                <option value=''>-- Chọn quận/huyện --</option>
-                {districts.map(district => (
-                    <option key={district.code} value={district.code}>{district.DistrictName}</option>
-                ))}
-            </select>
-            <select id='wards'>
-                <option value=''>-- Chọn phường/xã --</option>
-                {wards.map(ward => (
-                    <option key={ward.code} value={ward.code}>{ward.WardName}</option>
-                ))}
-            </select>
-        </div>
+        <>
+            <div className="">
+                <label className="form-label my-3">
+                    Tỉnh/ Thành <sup>*</sup>
+                </label>
+                <select id='provinces' className='form-control' onChange={handleProvinceChange}>
+                    <option value=''>-- Chọn tỉnh/thành phố --</option>
+                    {provinces.map(province => (
+                        <option key={province.code} value={province.ProvinceID}>{province.ProvinceName}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="">
+                <label className="form-label my-3">
+                    Quận/ Huyện <sup>*</sup>
+                </label>
+                <select id='districts' className='form-control' onChange={handleDistrictChange}>
+                    <option value=''>-- Chọn quận/huyện --</option>
+                    {districts.map(district => (
+                        <option key={district.code} value={district.DistrictID}>{district.DistrictName}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="">
+                <label className="form-label my-3">
+                    Phường/ Xã <sup>*</sup>
+                </label>
+                <select id='wards' className='form-control'>
+                    <option value=''>-- Chọn phường/xã --</option>
+                    {wards.map(ward => (
+                        <option key={ward.code} value={ward.code}>{ward.WardName}</option>
+                    ))}
+                </select>
+            </div>
+        </>
     );
 }
 
--- src/components/user/checkout/CheckoutPage.jsx
+++ src/components/user/checkout/CheckoutPage.jsx
@@ -11,97 +11,44 @@ function CheckoutPage() {
                     <form action="#">
                         <div className="row g-5">
                             <div className="col-md-12 col-lg-6 col-xl-7">
-                                <div className="row">
-                                    <div className="col-md-12 col-lg-6">
-                                        <div className="form-item w-100">
-                                            <label className="form-label my-3">
-                                                First Name<sup>*</sup>
-                                            </label>
-                                            <input type="text" className="form-control" />
-                                        </div>
-                                    </div>
-                                    <div className="col-md-12 col-lg-6">
-                                        <div className="form-item w-100">
-                                            <label className="form-label my-3">
-                                                Last Name<sup>*</sup>
-                                            </label>
-                                            <input type="text" className="form-control" />
-                                        </div>
-                                    </div>
-                                </div>
                                 <div className="form-item">
                                     <label className="form-label my-3">
-                                        Company Name<sup>*</sup>
+                                        Họ và tên<sup>*</sup>
                                     </label>
                                     <input type="text" className="form-control" />
                                 </div>
                                 <div className="form-item">
                                     <label className="form-label my-3">
-                                        Address <sup>*</sup>
+                                        Số điện thoại <sup>*</sup>
                                     </label>
                                     <input
                                         type="text"
                                         className="form-control"
-                                        placeholder="House Number Street Name"
+                                    />
+                                </div>
+                                <div className="form-item">
+                                    <label className="form-label my-3">
+                                        Email <sup>*</sup>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
                                     />
                                 </div>
                                 <Address />
                                 <div className="form-item">
                                     <label className="form-label my-3">
-                                        Town/City<sup>*</sup>
+                                        Số nhà/ Tên đường <sup>*</sup>
                                     </label>
-                                    <input type="text" className="form-control" />
-                                </div>
-                                <div className="form-item">
-                                    <label className="form-label my-3">
-                                        Country<sup>*</sup>
-                                    </label>
-                                    <input type="text" className="form-control" />
-                                </div>
-                                <div className="form-item">
-                                    <label className="form-label my-3">
-                                        Postcode/Zip<sup>*</sup>
-                                    </label>
-                                    <input type="text" className="form-control" />
-                                </div>
-                                <div className="form-item">
-                                    <label className="form-label my-3">
-                                        Mobile<sup>*</sup>
-                                    </label>
-                                    <input type="tel" className="form-control" />
-                                </div>
-                                <div className="form-item">
-                                    <label className="form-label my-3">
-                                        Email Address<sup>*</sup>
-                                    </label>
-                                    <input type="email" className="form-control" />
-                                </div>
-                                <div className="form-check my-3">
                                     <input
-                                        type="checkbox"
-                                        className="form-check-input"
-                                        id="Account-1"
-                                        name="Accounts"
-                                        defaultValue="Accounts"
+                                        type="text"
+                                        className="form-control"
                                     />
-                                    <label className="form-check-label" htmlFor="Account-1">
-                                        Create an account?
-                                    </label>
-                                </div>
-                                <hr />
-                                <div className="form-check my-3">
-                                    <input
-                                        className="form-check-input"
-                                        type="checkbox"
-                                        id="Address-1"
-                                        name="Address"
-                                        defaultValue="Address"
-                                    />
-                                    <label className="form-check-label" htmlFor="Address-1">
-                                        Ship to a different address?
-                                    </label>
                                 </div>
                                 <div className="form-item">
+                                    <label className="form-label my-3">
+                                        Thông tin thêm <sup>*</sup>
+                                    </label>
                                     <textarea
                                         name="text"
                                         className="form-control"
